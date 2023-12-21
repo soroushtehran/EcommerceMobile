b24bf81ad8b7dda4161cc379e89c6bb40123402c
Soroush Tehrani <soroush.art5@gmail.com>
2023-12-22 00:45:56 +0330

    FEATURE: Add Footer

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -6,12 +6,14 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
