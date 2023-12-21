c90d0a73ffcf9998042ebe540f4f1730c36ccebf
Soroush Tehrani <soroush.art5@gmail.com>
2023-12-21 23:12:52 +0330

    FEATURE: Add Initial Front-End Structure

---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import "./styles.css";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+function App() {
+  return (
+    <div>
+      Ecommerce Mobile
+    </div>
+  );
+}
+
+export default App;
---
+++ frontEnd/tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        // padding: '1'
+      },
+      colors: {
+        primary: "#B28AF8",
+        secondary: "#C19EFF",
+        button: "#FFFFFF",
+        buttonHover: "",
+        primaryDark: "#2B0075",
+        buttonDark: "#F069FF",
+        buttonDarkHover: "#B315C5",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemiBold: "Dana DemiBold",
+        Morabba: "Morabba",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+      animation: {
+        shadowLamp: "shadowLamp 2s ease 3s forwards",
+      },
+      keyframes: {
+        shadowLamp: {
+          "0%": {
+            boxShadow: "0 0px 0px transparent",
+          },
+          "50%": {
+            boxShadow: "0 1px 1px #ddd6fe7a",
+          },
+          "100%": {
+            boxShadow: "0 3px 3px #fff",
+          },
+        },
+      },
+      transitionDelay: {
+        2000: {
+          transitionDelay: "2000ms",
+        },
+      },
+      transitionDuration:{
+        2000: {
+          transitionDuration: "2000ms",
+        },
+        3000: {
+          transitionDuration: "3000ms",
+        },
+      },
+      outlineWidth : {
+        3 : '3px'
+      }
+    },
+  },
+  plugins: [
+    require("flowbite/plugin"),
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
+};
